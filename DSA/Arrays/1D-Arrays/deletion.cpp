310d4a653d67e4db672856c6bc6b4f573d7dd019
Inam05 <inamulhassan.tech@gmail.com>
2024-08-06 10:22:45 +0500

    Deletion at the specific point in an array

--- DSA/Arrays/1D-Arrays/deletion.cpp
+++ DSA/Arrays/1D-Arrays/deletion.cpp
@@ -40,6 +40,29 @@ int main ()
     }
     */
 
+    // Deletion at a specific position
+    int pos;
+    cout << "Enter the position of the element to be deleted (1-based index): ";
+    cin >> pos;
+
+    if (pos < 1 || pos > n)
+    {
+        cout << "Invalid position!" << endl;
+        return 1;
+    }
+
+    for (int i = pos - 1; i < n - 1; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
     
+    n--;
+
+    cout << "Array after deletion: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
