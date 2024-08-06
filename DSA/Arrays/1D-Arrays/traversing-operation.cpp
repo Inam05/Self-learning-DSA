8db7b36b9f42f7c47fcf90dcc11101dc9387610c
Inam05 <inamulhassan.tech@gmail.com>
2024-08-06 07:06:01 +0500

    Traversal Operation in an array concept has been added

---
+++ DSA/Arrays/1D-Arrays/traversing-operation.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    // Traversal Operation in an array
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    
+return 0;
+}
