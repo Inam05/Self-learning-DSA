0ba7131ce96d74a4faad4d56e022a713b1f767cb
Inam05 <inamulhassan.tech@gmail.com>
2024-08-06 10:03:01 +0500

    Deletion at the end of an array

--- DSA/Arrays/1D-Arrays/deletion.cpp
+++ DSA/Arrays/1D-Arrays/deletion.cpp
@@ -4,16 +4,20 @@ int main ()
 {
     // Deletion element in an array
 
-    // Deletion at beginning
+    
     int n;
     cout << "Enter the size of the array: ";
     cin >> n;
     int arr[n];
     cout << "Enter the elements of the array: ";
-    for (int i = 0; i < n; i++)
+    int i;
+    for (i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
+
+    /*
+    // Deletion at beginning
     for (int i = 0; i < n-1; i++)
     {
         arr[i] = arr[i+1];
@@ -24,5 +28,18 @@ int main ()
     {
         cout << arr[i] << " ";
     }
+    */
+
+    /*
+    // Deletion at the end of an array
+    n--;
+    cout << "Array after deletion : ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    */
+
+    
     return 0;
 }
