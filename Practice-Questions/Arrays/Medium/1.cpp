8850fcba49e791e82b82c4e7e808d66a8f096b55
Inam05 <inamulhassan.tech@gmail.com>
2024-08-09 11:11:52 +0500

    Find missing the smallest number in the n number of sorted array

---
+++ Practice-Questions/Arrays/Medium/1.cpp
@@ -0,0 +1,32 @@
+/* Given a sorted array of n distinct integers where each integer is in the range from 0 to m-1 and m > n. Find the smallest number that is missing from the array.  */
+// C++ program to find the smallest elements
+// missing in a sorted array.
+
+#include <iostream>
+using namespace std;
+
+int findFirstMissing(int array[], int start, int end)
+{
+    if (start > end)
+    {
+        return end + 1;
+    }
+    if (start != array[start])
+    {
+        return start;
+    }
+    int mid = (start + end) / 2;
+
+    if (array[mid] == mid)
+    {
+        return findFirstMissing(array, mid + 1, end);
+    }
+    return findFirstMissing(array, start, mid);
+}
+
+int main()
+{
+    int arr[] = {0, 1, 2, 3, 4, 5, 6, 8, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Smallest missing element is " << findFirstMissing(arr, 0, n - 1) << endl;
+}
